4816c87bf31d23363552a3fcfffcafd23fa870f6
Vinícius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2026-08-03 23:02:55 -0300

    📱 Web App funcional: cadastro, anunciar, PWA, busca, filtros

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
